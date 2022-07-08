492e0b97d4b54aabd211d42a5eacf4b34802e09f
Tomer Harari <tharari21@gmail.com>
2022-07-08 15:08:17 -0400

    commit tree c98caf6929ae9d4d0c01162fc9d3f30c8f57bf70

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  const title = document.querySelector("section#movieDetails h4");
+  const summary = document.querySelector("section#movieDetails p");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = e.target.children[1];
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          console.log("ERO");
+          title.textContent = "Error";
+          summary.textContent = `A movie with an id of ${input.value} does not exist`;
+        }
+      })
+      .then((data) => {
+        title.textContent = data.title;
+        summary.textContent = data.summary;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
